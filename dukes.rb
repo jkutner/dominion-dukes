87bdbddd1b6d65d3b9e5715440d6db45a4a48a45
jkutner <jpkutner@gmail.com>
2010-12-05 19:43:30 -0600

    changed hand to deck

--- dukes.rb
+++ dukes.rb
@@ -3,18 +3,18 @@ def score(dukes, duchies)
 end
 
 def play(dukes, duchies)
-  dukes_in_hand = 0
-  duchies_in_hand = 0
+  dukes_in_deck = 0
+  duchies_in_deck = 0
   
   while (dukes > 0 and duchies > 0)
-    if score(dukes_in_hand+1,  duchies_in_hand) >= score(dukes_in_hand, duchies_in_hand+1)
-      dukes_in_hand += 1
+    if score(dukes_in_deck+1,  duchies_in_deck) >= score(dukes_in_deck, duchies_in_deck+1)
+      dukes_in_deck += 1
       dukes -= 1
-      puts "duke ->   #{score(dukes_in_hand, duchies_in_hand)}"
+      puts "duke ->   #{score(dukes_in_deck, duchies_in_deck)}"
     else
-      duchies_in_hand += 1
+      duchies_in_deck += 1
       duchies -= 1
-      puts "duchy ->  #{score(dukes_in_hand, duchies_in_hand)}"
+      puts "duchy ->  #{score(dukes_in_deck, duchies_in_deck)}"
     end
   end
 end
